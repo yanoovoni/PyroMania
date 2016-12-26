17d5b29bf25531c36d4887fdc227b108299395d1
yanoovoni <yanooni@gmail.com>
2016-12-26 16:06:13 +0200

    Added a document folder and a design document

---
+++ PyroMania/Assets/Scripts/Characters/Bomber.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomber : MonoBehaviour {
+    protected int XPos; // The X position of the bomber on the map
+    protected int YPos; // The Y position of the bomber on the map
+    protected int MaxBombs; // The Maximum amount of bombs that the bomber can place
+    protected int MaxBombsBombCount; // The amount of bombs that the bomber has left
+    protected int Lives; // The amount of lives that the bomber has left
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+    
+    // Sets the bomber position to the specified location on the map
+    public void SetPosition(int XPos, int YPos) {
+        this.XPos = XPos;
+        this.YPos = YPos;
+    }
+
+    // Returns an array that has the location of the bomber on the map in this format [X, Y]
+    public int[] GetPosition() {
+        return new int[] {this.XPos, this.YPos};
+    }
+
+    // Sets the maximum amount of bombs that the bomber has to the specified amount
+    public void SetMaxBombs(int MaxBombs) {
+        this.MaxBombs = MaxBombs;
+    }
+
+    // Returns the maximum amount of bombs that the bomber has
+    public int GetMaxBombs() {
+        return this.MaxBombs;
+    }
+
+    // Sets the amount of lives that the bomber has to the specified amount
+    public void SetLives(int Lives) {
+        this.Lives = Lives;
+    }
+
+    // Returns the amount of lives that the bomber has
+    public int GetLives() {
+        return this.Lives;
+    }
+
+    // Hits the bomber - reduces his lives by 1, checks if he dies and triggers invulnerability effect
+    public void GetHit() {
+        //TODO
+    }
+
+    public void PlaceBomb() {
+        Map.Instance.
+    }
+
+}
---
+++ PyroMania/Assets/Scripts/Map/Map.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+
+public class Map : Singleton<Map> {
+    protected Map() {} // guarantee this will be always a singleton only - can't use the constructor!
+    protected int[,] MapInfo; // A 2d array containing all of the map's information
+    protected bool Visible; // Determines if the map is visible or not
+    
+    // Use this for initialization
+    void Start () {
+        this.Visible = false;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (this.IsVisible()) {
+            
+        }
+    }
+
+    // Loads the map from the file in the given location.
+    public bool LoadMap(string MapName) {
+        try {
+            string[] NewLine = { "\r\n" };
+            TextAsset MapFile = Resources.Load("Maps/" + MapName) as TextAsset;
+            if (MapFile == null) {
+                print("Map file not found");
+                return false;
+            }
+            string[] MapFileLines = MapFile.text.Split(NewLine, StringSplitOptions.RemoveEmptyEntries);
+            string[] StringMapSize = MapFileLines[0].Split(' ');
+            int[] MapSize = { int.Parse(StringMapSize[0]), int.Parse(StringMapSize[1]) };
+            this.MapInfo = new int[MapSize[0], MapSize[1]];
+            for (int i = 0; i < MapSize[0]; i++) {
+                string[] MapFileLine = MapFileLines[i + 1].Split(' ');
+                for (int i2 = 0; i2 < MapSize[1]; i2++) {
+                    this.MapInfo[i, i2] = int.Parse(MapFileLine[i2]);
+                }
+            }
+            Resources.UnloadAsset(MapFile);
+            return true;
+        } catch (IndexOutOfRangeException) {
+            Debug.Log("Map file corrupt");
+            return false;
+        }
+    }
+
+    // Sets the visibility of the map
+    public void SetVisibility(bool Visible) {
+        this.Visible = Visible;
+    }
+
+    // Returns if the map is visible
+    public bool IsVisible() {
+        return this.Visible;
+    }
+
+    // Changes a specified tile on the map to the specified value
+    public void SetTile(int XPos, int YPos, int Value) {
+        this.MapInfo[XPos, YPos] = Value;
+    }
+
+    // Returns the value of a specified tile
+    public int GetTile(int XPos, int YPos) {
+        return this.MapInfo[XPos, YPos];
+    }
+
+    public void PlaceBomb(int XPos, int YPos) {
+        //TODO
+    }
+}
