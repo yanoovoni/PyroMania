a506c78ad77f72651cae799924770304062aed52
yanoovoni <yanooni@gmail.com>
2017-01-31 04:04:57 +0200

    added basic classes to the server

--- Server/Server/NetworkManager.cs
+++ Server/Server/NetworkManager.cs
@@ -1,14 +1,10 @@
-﻿#define DEBUG
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server {
-    class Program {
-        static void Main(string[] args) {
-        }
+    class NetworkManager {
     }
 }
---
+++ Server/Server/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server {
+    static class Printer {
+        public static void Print(string message, [CallerMemberName]string callerName = "") {
+#if (DEBUG)
+            Console.WriteLine("{0}|{1}: {2}", DateTime.Now.ToString("hh:mm:ss"), callerName, message);
+#endif
+        }
+    }
+}
